e551fe51caa4c2125bdd35a169e359c4a03eb81d
Asiel Posada <asiel.posada@gmail.com>
2026-05-05 16:29:27 -0600

    Add Swift LSP support and Overseer plugin for task execution

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>tr", "<cmd>OverseerRun<cr>", desc = "Run Task" },
+      { "<leader>tt", "<cmd>OverseerToggle<cr>", desc = "Toggle Tasks" },
+    },
+    opts = {},
+  },
+}
---
+++ lua/plugins/swift.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sourcekit = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "swift" })
+    end,
+  },
+}
